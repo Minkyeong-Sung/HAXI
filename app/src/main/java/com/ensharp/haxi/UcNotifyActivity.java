d8098b4120c5f04214bee426be457171ca1124af
성민경 <zoz7@naver.com>
2016-07-26 21:06:02 +0900

    [민경] android:onClick 오류 수정 및 manifests 권한 오류 수정(buil.gradle 설정을 바꿔주니 해결됨 -> res.styles에서 thema 부분 지웠음) 그리고, 신고내용 다듬어서 수정 완료함.

--- app/src/main/java/com/ensharp/haxi/UcNotifyActivity.java
+++ app/src/main/java/com/ensharp/haxi/UcNotifyActivity.java
@@ -9,34 +9,52 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class UcNotifyActivity extends Activity {
-    Context mContext;
+    private  Context mContext;
+
     EditText name, phone, address;
+    Button btn_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_uc_notify);
 
-        mContext = this;
+
+//
+//        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR);
+//
+//        if(permissionCheck== PackageManager.PERMISSION_DENIED){
+//            // 권한 없음
+//            Toast.makeText(this, "권한없음", Toast.LENGTH_SHORT).show();
+//
+//        }else{
+//            // 권한 있음
+//            Toast.makeText(this, "권한있음", Toast.LENGTH_SHORT).show();
+//        }
+
+
+        mContext = UcNotifyActivity.this;
 
         name = (EditText)findViewById(R.id.smsText);
         phone = (EditText)findViewById(R.id.smsText2);
         address = (EditText)findViewById(R.id.smsText3);
-
     }
 
+
     public void sendSMS(View v){
-
-
         String strName = name.getText().toString();
         String strPhone = phone.getText().toString();
         String strAddress = address.getText().toString();
-        String total = strName + '\n' + strPhone + '\n' + strAddress;
+        String content = "저는 위 영수증 사진과 같이 왕십리에서 탑승하여 세종대학교에 도착하였으나, 경로를 둘러서 간 것 같으니, 확인 부탁드립니다.";
 
+        String total = "신고자" + strName + '\n' + "연락처" + strPhone + '\n' + "주소" + strAddress + '\n' + content ;
 
         if (total.length()>0 ){ //smsNum.length()>0 &&smsText.length()>0
             sendSMS("01048862255", total);
@@ -49,47 +67,37 @@ public class UcNotifyActivity extends Activity {
         PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT_ACTION"), 0);
         PendingIntent deliveredIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED_ACTION"), 0);
 
-
         registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 switch(getResultCode()){
                     case Activity.RESULT_OK:
-
                         Toast.makeText(mContext, "전송 완료", Toast.LENGTH_SHORT).show();
                         break;
                     case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-
                         Toast.makeText(mContext, "전송 실패", Toast.LENGTH_SHORT).show();
                         break;
                     case SmsManager.RESULT_ERROR_NO_SERVICE:
-
                         Toast.makeText(mContext, "서비스 지역이 아닙니다", Toast.LENGTH_SHORT).show();
                         break;
                     case SmsManager.RESULT_ERROR_RADIO_OFF:
-
                         Toast.makeText(mContext, "무선(Radio)가 꺼져있습니다", Toast.LENGTH_SHORT).show();
                         break;
                     case SmsManager.RESULT_ERROR_NULL_PDU:
-
                         Toast.makeText(mContext, "PDU Null", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
         }, new IntentFilter("SMS_SENT_ACTION"));
 
-
-
         registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 switch (getResultCode()){
                     case Activity.RESULT_OK:
-
                         Toast.makeText(mContext, "SMS 도착완료", Toast.LENGTH_SHORT).show();
                         break;
                     case Activity.RESULT_CANCELED:
-
                         Toast.makeText(mContext, "SMS 도착안됨", Toast.LENGTH_SHORT).show();
                         break;
                 }
@@ -98,6 +106,13 @@ public class UcNotifyActivity extends Activity {
 
         SmsManager mSmsManager = SmsManager.getDefault();
         mSmsManager.sendTextMessage("01048862255", null, total, sentIntent, deliveredIntent);
+
+        // 지정 글자수 넘어갔을때 mms로 보내도록 //
+        ArrayList<String> messageParts = mSmsManager.divideMessage(total);
+
+        mSmsManager.sendMultipartTextMessage("01048862255", null, messageParts, null, null);
+
+        Toast.makeText(this, "전송완료.", Toast.LENGTH_SHORT).show();
 
     }
 }
