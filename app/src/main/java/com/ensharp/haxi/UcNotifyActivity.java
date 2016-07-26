a45d2a3ca7624966ff16ce16ed0ac7d1c0d7ffa5
윤명식 <yms0059@naver.com>
2016-07-26 20:44:54 +0900

    [명식] 텍스트 문자 -> 영수증 사진 촬영 후 문자 전송

--- app/src/main/java/com/ensharp/haxi/UcNotifyActivity.java
+++ app/src/main/java/com/ensharp/haxi/UcNotifyActivity.java
@@ -1,13 +1,43 @@
 package com.ensharp.haxi;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class UcNotifyActivity extends Activity {
+
+     Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uc_notify);
+
+        next = (Button)findViewById(R.id.btn_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent notify2Intent = new Intent(UcNotifyActivity.this,UcNotifyActivity2.class);
+                startActivity(notify2Intent);
+            }
+        });
     }
 }
