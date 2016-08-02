526bd72e18098f53824a0bc343bc6ca5b3403d59
박민현 <algusl92@naver.com>
2016-08-02 14:50:56 +0900

    [민현] 택시비 , 거리 정보를 알려주는 Diaglog 창에서 누적거리 Activity 안 떳던 예외처리 해결

--- app/src/main/java/com/ensharp/haxi/UcMainActivity.java
+++ app/src/main/java/com/ensharp/haxi/UcMainActivity.java
@@ -2,6 +2,7 @@ package com.ensharp.haxi;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -339,7 +340,7 @@ public class UcMainActivity extends Activity {
                     .setPositiveButton("누적거리 시작", new DialogInterface.OnClickListener(){
                         @Override
                         public void onClick(DialogInterface dialog, int id) {
-                            Intent runningIntent = new Intent(UcMainActivity.this, UcRunningActivity.class);
+                            Intent runningIntent = new Intent(((Dialog) dialog).getContext(), UcRunningActivity.class);
                             startActivity(runningIntent);
                         }
                     })
