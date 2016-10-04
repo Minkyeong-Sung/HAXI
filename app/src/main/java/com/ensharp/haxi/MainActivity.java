1733b3256b608033c45e687d0f4a3b630f1216ce
안태현 <antaehyeon@live.co.kr>
2016-10-05 07:11:51 +0900

    1. 앱 로고 변경 2. Splash Activity Image 변경 3. MainActivity 변경 (미완성) 4. ImageView 안보이는 Bug Fix

--- app/src/main/java/com/ensharp/haxi/MainActivity.java
+++ app/src/main/java/com/ensharp/haxi/MainActivity.java
@@ -1,15 +1,12 @@
 package com.ensharp.haxi;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
-import com.ensharp.haxi.InternationalTaxi.InternationalTaxiActivity;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     Button openUc_activity;
     Button openIT_activity;
@@ -22,21 +19,21 @@ public class MainActivity extends AppCompatActivity {
         // 스플래시 화면 띄우는 부분
         startActivity(new Intent(this, SplashActivity.class));
 
-        openUc_activity = (Button)findViewById(R.id.btn_openUcActivity);
-        openUc_activity.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent UcIntent = new Intent(MainActivity.this, UcMainActivity.class);
-                startActivity(UcIntent);
-            }
-        });
-
-        openIT_activity = (Button)findViewById(R.id.btn_openInternationalTaxiActivity);
-        openIT_activity.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent internationalIntent = new Intent(MainActivity.this, InternationalTaxiActivity.class);
-                startActivity(internationalIntent);
-            }
-        });
+//        openUc_activity = (Button)findViewById(R.id.btn_openUcActivity);
+//        openUc_activity.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v){
+//                Intent UcIntent = new Intent(MainActivity.this, UcMainActivity.class);
+//                startActivity(UcIntent);
+//            }
+//        });
+//
+//        openIT_activity = (Button)findViewById(R.id.btn_openInternationalTaxiActivity);
+//        openIT_activity.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v){
+//                Intent internationalIntent = new Intent(MainActivity.this, InternationalTaxiActivity.class);
+//                startActivity(internationalIntent);
+//            }
+//        });
     }
 }
 
